13c25ee920345d9600171aeb2fd99c3cd9927963
JustMax7CB <maximsh4@ac.sce.ac.il>
2021-06-21 15:11:19 +0300

    Created a function to color a certain pygame.rect object, by storing an list pair in the array of (cell, cellcolor) and when changing the color, filling the cell with the requested color, and changing the second variable in the list to the requested color

--- Main.py
+++ Main.py
@@ -1,15 +1,14 @@
 import pygame
-
-ALIVE = WHITE = (255, 255, 255)  # WHITE
-DEAD = BLACK = (0, 0, 0)  # BLACK
-CellSize = 40
-WindowDimensions = (10 * CellSize, 5 * CellSize)
+DEAD = WHITE = (255, 255, 255)  # WHITE
+ALIVE = BLACK = (0, 0, 0)  # BLACK
+ABOUT_TO_DIE = (56, 69, 69)  # GRAYISH
+CellSize = 20
+WindowDimensions = (40 * CellSize, 20 * CellSize)
 BoardArr = [[None for i in range(int(WindowDimensions[0] / CellSize))] for j in
             range(int(WindowDimensions[1] / CellSize))]
 Window = None
 
 print(len(BoardArr), ", ", len(BoardArr[0]))
-print(BoardArr)
 
 
 def StartUpWindow():
@@ -22,16 +21,16 @@ def StartUpWindow():
 def draw_grid():
     Column, Row = 0, 0
 
-    def AddCellToArray(cell):
+    def AddCellToArray(cell, cellColor):
         nonlocal Column, Row
         if Row < len(BoardArr):
             if Column < len(BoardArr[0]):
-                BoardArr[Row][Column] = cell
+                BoardArr[Row][Column] = [cell, cellColor]
                 Column += 1
             else:
                 Row += 1
                 Column = 0
-                BoardArr[Row][Column] = cell
+                BoardArr[Row][Column] = [cell, cellColor]
                 Column += 1
         else:
             print("Error adding cell to array")
@@ -39,22 +38,81 @@ def draw_grid():
     for y in range(0, WindowDimensions[1], CellSize):
         for x in range(0, WindowDimensions[0], CellSize):
             cell = pygame.Rect(x, y, CellSize, CellSize)
-            AddCellToArray(cell)
-            pygame.draw.rect(surface=Window, color=BLACK, rect=cell, width=1)
+            cellColor = WHITE
+            AddCellToArray(cell, cellColor)
+            pygame.draw.rect(surface=Window, color=cellColor, rect=cell, width=0)
 
 
-def ColorCell():
-    Window.fill(BLACK, BoardArr[2][4])
-    Window.fill(BLACK, BoardArr[4][9])
+def test(cell):
+    # if Alive and has 0/1 neighbors - Dead
+    # if Alive and has 4+ neighbors - Dead
+    # if Alive and has 2/3 neighbors - Alive
+    # if Dead adn has exactly 3 neighbors - Alive
+    NeighborsSum, COLUMN, ROW = 0, 0, 0
+    for column in range(len(BoardArr[0])):
+        for row in range(len(BoardArr)):
+            if BoardArr[row][column] == cell:
+                ROW = row
+                COLUMN = column
+                print(cell)
+                print(BoardArr[row][column])
+                break
+    print(ROW, COLUMN)
+    for column in range(len(BoardArr[0])):
+        for row in range(len(BoardArr)):
+            if row == ROW - 1 and (column == COLUMN or column == COLUMN - 1 or column == COLUMN + 1):
+                if BoardArr[row][column][1] == ALIVE:
+                    NeighborsSum += 1
+            elif row == ROW + 1 and (column == COLUMN or column == COLUMN - 1 or column == COLUMN + 1):
+                if BoardArr[row][column][1] == ALIVE:
+                    NeighborsSum += 1
+            elif row == ROW and (column == COLUMN - 1 or column == COLUMN + 1):
+                if BoardArr[row][column][1] == ALIVE:
+                    NeighborsSum += 1
+    print(row, column, NeighborsSum)
+    if cell[1] == ALIVE and (NeighborsSum == 0 or NeighborsSum == 1):
+        ColorCell(cell, DEAD)
+    elif cell[1] == ALIVE and (NeighborsSum > 3):
+        ColorCell(cell, DEAD)
+    elif cell[1] == ALIVE and (NeighborsSum == 2 or NeighborsSum == 3):
+        ColorCell(cell, ALIVE)
+    elif cell[1] == DEAD and NeighborsSum == 3:
+        ColorCell(cell, ALIVE)
+
+
+def ColorCell(cell, color):
+    Window.fill(color, cell[0])
+    cell[1] = color
+
+
+def play():
+    for row in range(len(BoardArr[0])):
+        for column in range(len(BoardArr)):
+            test(BoardArr[column][row])
+def start():
+    ColorCell(BoardArr[1][2], ALIVE)
+    ColorCell(BoardArr[1][3], ALIVE)
+    ColorCell(BoardArr[2][1], ALIVE)
+    ColorCell(BoardArr[3][1], ALIVE)
+    ColorCell(BoardArr[2][4], ALIVE)
+    ColorCell(BoardArr[3][3], ALIVE)
+    ColorCell(BoardArr[4][2], ALIVE)
+    ColorCell(BoardArr[4][3], ALIVE)
+    ColorCell(BoardArr[4][4], ALIVE)
+    ColorCell(BoardArr[5][2], ALIVE)
+    ColorCell(BoardArr[4][1], ALIVE)
 
 
 def main():
+    #clock = pygame.time.Clock()
     pygame.init()
     StartUpWindow()
     run = True
     while run:
+        #clock.tick(FPS)
         draw_grid()
-        ColorCell()
+        start()
+        play()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
