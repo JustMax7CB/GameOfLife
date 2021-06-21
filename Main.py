c86b9bf1c272805ec18e4296f22daab0f607e292
JustMax7CB <maximsh4@ac.sce.ac.il>
2021-06-21 13:00:04 +0300

    Functions draw grid and add cell fixed now the grid represented as an x,y axis as needed

--- Main.py
+++ Main.py
@@ -2,13 +2,15 @@ import pygame
 
 ALIVE = WHITE = (255, 255, 255)  # WHITE
 DEAD = BLACK = (0, 0, 0)  # BLACK
-CellSize = 15
-WindowDimensions = (50 * CellSize, 20 * CellSize)
-BoardArr = [[None for i in range(int(WindowDimensions[0]/CellSize))] for j in range(int(WindowDimensions[1]/CellSize))]
+CellSize = 40
+WindowDimensions = (10 * CellSize, 5 * CellSize)
+BoardArr = [[None for i in range(int(WindowDimensions[0] / CellSize))] for j in
+            range(int(WindowDimensions[1] / CellSize))]
 Window = None
 
 print(len(BoardArr), ", ", len(BoardArr[0]))
 print(BoardArr)
+
 
 def StartUpWindow():
     global Window
@@ -18,34 +20,32 @@ def StartUpWindow():
 
 
 def draw_grid():
-    ArrayX, ArrayY = 0, 0
+    Column, Row = 0, 0
 
-    def AddCell(cell):
-        nonlocal ArrayY, ArrayX
-        if ArrayX < len(BoardArr):
-            if ArrayY < len(BoardArr[ArrayX]):
-                BoardArr[ArrayX][ArrayY] = cell
-                ArrayY += 1
+    def AddCellToArray(cell):
+        nonlocal Column, Row
+        if Row < len(BoardArr):
+            if Column < len(BoardArr[0]):
+                BoardArr[Row][Column] = cell
+                Column += 1
             else:
-                ArrayX += 1
-                ArrayY = 0
-                BoardArr[ArrayX][ArrayY] = cell
-                ArrayY += 1
+                Row += 1
+                Column = 0
+                BoardArr[Row][Column] = cell
+                Column += 1
         else:
             print("Error adding cell to array")
 
-    for x in range(0, WindowDimensions[0], CellSize):
-        for y in range(0, WindowDimensions[1], CellSize):
+    for y in range(0, WindowDimensions[1], CellSize):
+        for x in range(0, WindowDimensions[0], CellSize):
             cell = pygame.Rect(x, y, CellSize, CellSize)
-            AddCell(cell)
+            AddCellToArray(cell)
             pygame.draw.rect(surface=Window, color=BLACK, rect=cell, width=1)
 
 
 def ColorCell():
-    for i in range(5):
-        Window.fill(BLACK, BoardArr[i][0])
-
-
+    Window.fill(BLACK, BoardArr[2][4])
+    Window.fill(BLACK, BoardArr[4][9])
 
 
 def main():
